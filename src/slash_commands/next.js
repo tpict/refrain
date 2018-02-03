c7a08e2538e8d3fe7bf959d88fc7ddde8febcba3
Tom Picton <tom@tompicton.co.uk>
2018-02-03 17:41:02 +0000

    Fix /next

--- src/slash_commands/next.js
+++ src/slash_commands/next.js
@@ -1,75 +1,70 @@
 const User = require('../models/user');
 const utils = require('../utils');
 const { wrapper } = require('./permission_wrapper');
+const logger = require('../logger');
 
-module.exports = wrapper(async function next(req, res) {
-  const activeUser = await User.getActive();
-  const spotifyApi = await activeUser.getSpotifyApi();
+async function performSkip(spotifyApi, req) {
   const webClient = utils.getWebClient();
 
-  const callback = (skippedName, skippedArtist, errorText) => {
-    let skipText = errorText;
-    if (!skipText) {
-      skipText =
-        skippedName == 'Rattlesnake'
-        ? 'You are weak. :snake:'
-        : `Skipping ${utils.formatSong(skippedName, skippedArtist)}...`;
-    }
-    utils.respond(req, res, skipText);
+  try {
+    await spotifyApi.skipToNext();
+  } catch (err) {
+    logger.error('Error skipping current track for /next: ' + err);
+    // TODO: Expose the generic error message function to use here
+    return webClient.chat.postMessage(
+      req.body.channel_id,
+      'Spotify couldn\'t skip this track!'
+    );
+  }
 
-    spotifyApi
-      .skipToNext()
-      .then(
-        async () => {
-          await utils.sleep(500);
-          return spotifyApi.getMyCurrentPlayingTrack();
-        },
-        err =>
-        utils.errorWrapper(err, errorMessage =>
-          webClient.chat.postMessage(
-            req.body.channel_id,
-            errorMessage || 'Spotify couldn\'t skip this track!'
-          )
-        )
-      )
-      .then(
-        data => {
-          const track = data.body.item;
+  await utils.sleep(500);
 
-          if (!track) {
-            webClient.chat.postMessage(
-              req.body.channel_id,
-              'Out of music! You might need to use `/playplaylist`.'
-            );
-            return;
-          }
+  let nextTrackData = {};
+  try {
+    nextTrackData = await spotifyApi.getMyCurrentPlayingTrack();
+  } catch (err) {
+    logger.error('Error getting current track post-skip for /next: ' + err);
+    // TODO: As above
+    return webClient.chat.postMessage(
+      req.body.channel_id,
+      'Managed to skip, but Spotify wouldn\'t say what\'s playing now!'
+    );
+  }
 
-          const name = track.name;
-          const artist = track.artists[0].name;
+  const track = nextTrackData.body.item;
 
-          webClient.chat.postMessage(
-            req.body.channel_id,
-            `Now playing ${utils.formatSong(name, artist)}`,
-            (err, res) => console.error(err, res)
-          );
-        },
-        err =>
-        utils.errorWrapper(err, errorMessage =>
-          webClient.chat.postMessage(
-            req.body.channel_id,
-            errorMessage ||
-            'Managed to skip, but Spotify wouldn\'t say what\'s playing now!'
-          )
-        )
-      );
-  };
+  if (!track) {
+    return webClient.chat.postMessage(
+      req.body.channel_id,
+      'Out of music! You might need to use `/playplaylist`.'
+    );
+  }
 
-  spotifyApi.getMyCurrentPlayingTrack().then(
-    data => {
-      const skippedName = data.body.item.name;
-      const skippedArtist = data.body.item.artists[0].name;
-      callback(skippedName, skippedArtist);
-    },
-    () => callback(null, null, 'Skipping whatever\'s playing...')
+  const name = track.name;
+  const artist = track.artists[0].name;
+
+  webClient.chat.postMessage(
+    req.body.channel_id,
+    `Now playing ${utils.formatSong(name, artist)}`
   );
+}
+
+module.exports = wrapper(async function next(req) {
+  const activeUser = await User.getActive();
+  const spotifyApi = await activeUser.getSpotifyApi();
+
+  let trackData = {};
+  let skipMessage = 'Skipping...';
+
+  try {
+    trackData = await spotifyApi.getMyCurrentPlayingTrack();
+    const skippedName = trackData.body.item.name;
+    const skippedArtist = trackData.body.item.artists[0].name;
+    skipMessage = `Skipping ${utils.formatSong(skippedName, skippedArtist)}...`;
+  } catch (err) {
+    logger.error('Error getting current track for /next: ' + err);
+  }
+
+  performSkip(spotifyApi, req);
+  return utils.slackAt(req, skipMessage);
 });
