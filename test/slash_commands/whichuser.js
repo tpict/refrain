18a2dc65a259c519ffe045209b59a49914ef1309
Tom Picton <tom@tompicton.co.uk>
2018-02-03 18:16:33 +0000

    Clean up /whichuser tests

--- test/slash_commands/whichuser.js
+++ test/slash_commands/whichuser.js
@@ -11,42 +11,36 @@ const User = require('../../src/models/user');
 chai.use(chaiHttp);
 
 describe('/whichuser endpoint', function () {
-  it('should prompt new users to use /spotifyauth', function (done) {
-    User.remove({}).then(() => {
-      const body = utils.baseSlackRequest({
-        command: '/whichuser'
-      });
-
-      chai
-        .request(app)
-        .post('/whichuser')
-        .send(body)
-        .end((err, res) => {
-          console.log(res);
-          chai.assert.equal(
-            res.body.text,
-            '<@U1AAAAAAA>: No authenticated users yet. Use `/spotifyauth` to get started.'
-          );
-          done();
-        });
-    });
-  });
-
-  it('should return the active user', function (done) {
+  it('should prompt new users to use /spotifyauth', async function () {
+    await User.remove({});
     const body = utils.baseSlackRequest({
       command: '/whichuser'
     });
 
-    chai
+    const res = await chai
       .request(app)
       .post('/whichuser')
-      .send(body)
-      .end((err, res) => {
-        chai.assert.equal(
-          res.body.text,
-          '<@U1AAAAAAA>: The active user is <@U1AAAAAAA>'
-        );
-        done();
-      });
+      .send(body);
+
+    chai.assert.equal(
+      res.body.text,
+      '<@U1AAAAAAA>: No authenticated users yet. Use `/spotifyauth` to get started.'
+    );
+  });
+
+  it('should return the active user', async function () {
+    const body = utils.baseSlackRequest({
+      command: '/whichuser'
+    });
+
+    const res = await chai
+      .request(app)
+      .post('/whichuser')
+      .send(body);
+
+    chai.assert.equal(
+      res.body.text,
+      '<@U1AAAAAAA>: The active user is <@U1AAAAAAA>'
+    );
   });
 });
