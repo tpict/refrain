73c1acd9f79ee9446764ed6f9e762a10563c65dc
Tom Picton <tom@tompicton.co.uk>
2018-02-03 18:12:28 +0000

    Fix /whichplaylist

--- src/slash_commands/whichplaylist.js
+++ src/slash_commands/whichplaylist.js
@@ -1,16 +1,17 @@
 const Playlist = require('../models/playlist');
 const utils = require('../utils');
 
-module.exports = async function whichplaylist(req, res) {
+module.exports = async function whichplaylist(req) {
   const activePlaylist = await Playlist.getActive();
 
   if (activePlaylist) {
-    utils.respond(
+    return utils.slackAt(
       req,
-      res,
-      `The active playlist is *${activePlaylist.name}*. If that's not what you're hearing, you'll have to select it from Spotify yourself.`
+      `The active playlist is *${
+        activePlaylist.name
+      }*. If that's not what you're hearing, you'll have to select it from Spotify yourself.`
     );
   } else {
-    utils.respond(req, res, 'There is no active playlist!', req);
+    return utils.slackAt(req, 'There is no active playlist!', req);
   }
 };
--- test/slash_commands/whichplaylist.js
+++ test/slash_commands/whichplaylist.js
@@ -11,49 +11,45 @@ const Playlist = require('../../src/models/playlist');
 chai.use(chaiHttp);
 
 describe('/whichplaylist endpoint', function () {
-  it('should tell the user if no playlist is active', function (done) {
+  it('should tell the user if no playlist is active', async function () {
     const body = utils.baseSlackRequest({
       command: '/whichplaylist'
     });
 
-    chai
+    const res = await chai
       .request(app)
       .post('/whichplaylist')
-      .send(body)
-      .end((err, res) => {
-        chai.assert.equal(
-          res.body.text,
-          '<@bing.bong>: There is no active playlist!'
-        );
-        chai.assert.equal(res.body.response_type, 'in_channel');
-        done();
-      });
+      .send(body);
+
+    chai.assert.equal(
+      res.body.text,
+      '<@U1AAAAAAA>: There is no active playlist!'
+    );
+    chai.assert.equal(res.body.response_type, 'in_channel');
   });
 
-  it('should tell the user which playlist is active', function (done) {
+  it('should tell the user which playlist is active', async function () {
     const playlist = new Playlist({
       spotifyID: 'P000000000000000000000',
       spotifyUserID: 'U1AAAAAAA',
       name: 'My playlist',
       active: true
     });
-    playlist.save().then(() => {
-      const body = utils.baseSlackRequest({
-        command: '/whichplaylist'
-      });
 
-      chai
-        .request(app)
-        .post('/whichplaylist')
-        .send(body)
-        .end((err, res) => {
-          chai.assert.equal(
-            res.body.text,
-            '<@bing.bong>: The active playlist is *My playlist*. If that\'s not what you\'re hearing, you\'ll have to select it from Spotify yourself.'
-          );
-          chai.assert.equal(res.body.response_type, 'in_channel');
-          done();
-        });
+    await playlist.save();
+    const body = utils.baseSlackRequest({
+      command: '/whichplaylist'
     });
+
+    const res = await chai
+      .request(app)
+      .post('/whichplaylist')
+      .send(body);
+
+    chai.assert.equal(
+      res.body.text,
+      '<@U1AAAAAAA>: The active playlist is *My playlist*. If that\'s not what you\'re hearing, you\'ll have to select it from Spotify yourself.'
+    );
+    chai.assert.equal(res.body.response_type, 'in_channel');
   });
 });
