tpict/refrain

commit a4cccad9fedba14a945c946016ea85dc84eb2732

Use more Track models where raw Spotify data was used

src/interactive/delete_track.js
@@ -10,11 +10,25 @@ module.exports = async function delete_track(payload) {
     return { text: 'Crisis averted.' };
   }
 
-  const track = JSON.parse(action.value);
-  const formattedSong = utils.formatSong(track.name, track.artist);
-  const playlist = await Playlist.getActive();
-  const activeUser = await User.getActive();
-  const spotifyApi = await activeUser.getSpotifyApi();
+  let playlist;
+  let track;
+  let spotifyApi;
+
+  await Promise.all([
+    new Promise(async resolve => {
+      playlist = await Playlist.getActive();
+      await playlist
+        .populate({ path: 'tracks', match: { spotifyID: action.value } })
+        .execPopulate();
+      track = playlist.tracks[0];
+      resolve();
+    }),
+    new Promise(async resolve => {
+      const activeUser = await User.getActive();
+      spotifyApi = await activeUser.getSpotifyApi();
+      resolve();
+    })
+  ]);
 
   try {
     spotifyApi.removeTracksFromPlaylist(
@@ -38,6 +52,6 @@ module.exports = async function delete_track(payload) {
 
   return utils.slackAt(
     payload.user.id,
-    `That bad? Let's not listen to ${formattedSong} again. :bomb:`
+    `That bad? Let's not listen to ${track.formattedTitle} again. :bomb:`
   );
 };

src/slash_commands/eradicate.js
@@ -6,9 +6,9 @@ module.exports = async function eradicate(req) {
   const activeUser = await User.getActive();
   const spotifyApi = await activeUser.getSpotifyApi();
 
-  let data;
+  let track;
   try {
-    data = await spotifyApi.getMyCurrentPlayingTrack();
+    track = await spotifyApi.refrain.getMyCurrentPlayingTrack(false);
   } catch (err) {
     logger.error(
       `Error getting current track for /eradicate: ${err.stack || err}`
@@ -16,22 +16,15 @@ module.exports = async function eradicate(req) {
     throw err;
   }
 
-  if (data.statusCode === 204) {
+  if (!track) {
     return utils.slackAt(
       req,
       'Are you hearing things? If so, you might want to use `/playplaylist` to try and re-sync things.'
     );
   }
 
-  const track = data.body.item;
-  const name = track.name;
-  const artist = track.artists[0].name;
-
   return utils.slackAt(req, {
-    text: `Whoa! Are you absolutely positive that you want to delete ${utils.formatSong(
-      name,
-      artist
-    )}?`,
+    text: `Whoa! Are you absolutely positive that you want to delete ${track.formattedTitle}?`,
     attachments: [
       {
         fallback: 'Your device doesn\'t support this.',
@@ -43,11 +36,7 @@ module.exports = async function eradicate(req) {
             text: 'Do it.',
             type: 'button',
             style: 'danger',
-            value: JSON.stringify({
-              uri: track.uri,
-              name,
-              artist
-            })
+            value: track.spotifyID,
           },
           {
             name: 'cancel',

test/interactive/delete_track.js
@@ -24,7 +24,7 @@ describe('/eradicate interactive callback', function () {
       .end((err, res) => {
         chai.assert.equal(
           res.body.text,
-          '<@U1AAAAAAA>: That bad? Let\'s not listen to *Test Me* by *Jme* again. :bomb:'
+          '<@U1AAAAAAA>: That bad? Let\'s not listen to *Mr. Brightside* by *The Killers* again. :bomb:'
         );
         removeTrackScope.done();
         nextTrackScope.done();

test/slash_commands/eradicate.js
@@ -32,8 +32,7 @@ describe('/eradicate endpoint', function () {
               text: 'Do it.',
               type: 'button',
               style: 'danger',
-              value:
-                '{"uri":"spotify:track:0eGsygTp906u18L0Oimnem","name":"Mr. Brightside","artist":"The Killers"}'
+              value: '0eGsygTp906u18L0Oimnem'
             },
             {
               name: 'cancel',

src/slash_commands/next.js
@@ -49,14 +49,12 @@ module.exports = async function next(req) {
   const activeUser = await User.getActive();
   const spotifyApi = await activeUser.getSpotifyApi();
 
-  let trackData = {};
+  let track;
   let skipMessage = 'Skipping...';
 
   try {
-    trackData = await spotifyApi.getMyCurrentPlayingTrack();
-    const skippedName = trackData.body.item.name;
-    const skippedArtist = trackData.body.item.artists[0].name;
-    skipMessage = `Skipping ${utils.formatSong(skippedName, skippedArtist)}...`;
+    track = await spotifyApi.refrain.getMyCurrentPlayingTrack(false);
+    skipMessage = `Skipping ${track.formattedTitle}...`;
   } catch (err) {
     logger.error(
       'Error getting current track for /next: ' + (err.stack || err)

src/slash_commands/permission_wrapper.js
@@ -1,5 +1,4 @@
 const utils = require('../utils');
-const logger = require('../logger');
 const Playlist = require('../models/playlist');
 const User = require('../models/user');
 
@@ -14,14 +13,6 @@ async function hasUsers() {
 async function hasPlaylists() {
   if ((await Playlist.count({})) === 0) {
     throw 'There are no configured playlists. Try `/addplaylist` to get started.';
-  }
-}
-
-// This shouldn't be necessary but is useful for testing.
-async function hasActivePlaylist() {
-  if (process.env.NODE_ENV !== 'production' && !await Playlist.getActive()) {
-    logger.error('No active playlist found. Please report this.');
-    throw 'For some reason, there\'s no active playlist. Try starting one with `/listplaylists.`';
   }
 }
 
@@ -45,7 +36,7 @@ module.exports = {
 
   async needsSetup(req, res, next) {
     try {
-      await Promise.all([hasUsers(), hasPlaylists(), hasActivePlaylist()]);
+      await Promise.all([hasUsers(), hasPlaylists()]);
       next();
     } catch (err) {
       res.send(utils.slackAt(req, err));

src/spotify_api.js
@@ -106,8 +106,9 @@ const extended = api => ({
   // counterparts, but accept and return Mongoose models.
 
   // Returns null, the Track item in the database (uses active playlist
-  // context) or a new Track.
-  getMyCurrentPlayingTrack: async () => {
+  // context) or a new Track. The lookup parameter defines whether or not to
+  // look up the track in the database.
+  getMyCurrentPlayingTrack: async (lookup = true) => {
     const res = await api.getMyCurrentPlayingTrack();
     const trackData = res.body.item;
 
@@ -115,16 +116,18 @@ const extended = api => ({
       return null;
     }
 
-    const playlist = await Playlist.getActive();
-    await playlist
-      .populate({
-        path: 'tracks',
-        match: { spotifyID: trackData.id }
-      })
-      .execPopulate();
+    if (lookup) {
+      const playlist = await Playlist.getActive();
+      await playlist
+        .populate({
+          path: 'tracks',
+          match: { spotifyID: trackData.id }
+        })
+        .execPopulate();
 
-    if (playlist.tracks.length > 0) {
-      return playlist.tracks[0];
+      if (playlist.tracks.length > 0) {
+        return playlist.tracks[0];
+      }
     }
 
     return new Track({

src/utils.js
@@ -91,10 +91,6 @@ module.exports = {
     return formatted;
   },
 
-  formatSong(trackName, artistName) {
-    return `*${trackName}* by *${artistName}*`;
-  },
-
   getWebClient() {
     return new WebClient(process.env.SLACK_API_TOKEN);
   },

commit 38973e213001844cc3b9c04e5ae696ba2921a051

Fix /eradicate
a4cccad caused problems with deleting tracks that were added directly
through Spotify

src/interactive/delete_track.js
@@ -1,5 +1,6 @@
-const User = require('../models/user');
 const Playlist = require('../models/playlist');
+const Track = require('../models/track');
+const User = require('../models/user');
 const utils = require('../utils');
 const logger = require('../logger');
 
@@ -10,17 +11,15 @@ module.exports = async function delete_track(payload) {
     return { text: 'Crisis averted.' };
   }
 
+  const trackData = JSON.parse(action.value);
+  const track = new Track(trackData);
+
   let playlist;
-  let track;
   let spotifyApi;
 
   await Promise.all([
     new Promise(async resolve => {
       playlist = await Playlist.getActive();
-      await playlist
-        .populate({ path: 'tracks', match: { spotifyID: action.value } })
-        .execPopulate();
-      track = playlist.tracks[0];
       resolve();
     }),
     new Promise(async resolve => {

src/slash_commands/eradicate.js
@@ -8,7 +8,7 @@ module.exports = async function eradicate(req) {
 
   let track;
   try {
-    track = await spotifyApi.refrain.getMyCurrentPlayingTrack(false);
+    track = await spotifyApi.refrain.getMyCurrentPlayingTrack();
   } catch (err) {
     logger.error(
       `Error getting current track for /eradicate: ${err.stack || err}`
@@ -36,7 +36,11 @@ module.exports = async function eradicate(req) {
             text: 'Do it.',
             type: 'button',
             style: 'danger',
-            value: track.spotifyID,
+            value: JSON.stringify({
+              spotifyID: track.spotifyID,
+              title: track.title,
+              artist: track.artist
+            })
           },
           {
             name: 'cancel',

test/interactive/delete_track.js
@@ -6,7 +6,7 @@ const chai = require('chai');
 const app = require('../../src/app');
 
 describe('/eradicate interactive callback', function () {
-  it('should delete tracks', function (done) {
+  it('should delete tracks', async function () {
     const removeTrackScope = nock('https://api.spotify.com')
       .delete('/v1/users/U1AAAAAAA/playlists/P000000000000000000000/tracks')
       .reply(200);
@@ -17,22 +17,20 @@ describe('/eradicate interactive callback', function () {
 
     const body = require('../fixtures/eradicate_delete.json');
 
-    chai
+    const res = await chai
       .request(app)
       .post('/interactive')
-      .send(body)
-      .end((err, res) => {
-        chai.assert.equal(
-          res.body.text,
-          '<@U1AAAAAAA>: That bad? Let\'s not listen to *Mr. Brightside* by *The Killers* again. :bomb:'
-        );
-        removeTrackScope.done();
-        nextTrackScope.done();
-        done();
-      });
+      .send(body);
+
+    chai.assert.equal(
+      res.body.text,
+      '<@U1AAAAAAA>: That bad? Let\'s not listen to *Test Me* by *Jme* again. :bomb:'
+    );
+    removeTrackScope.done();
+    nextTrackScope.done();
   });
 
-  it('should cancel track deletion', function (done) {
+  it('should cancel track deletion', async function () {
     const removeTrackScope = nock('https://api.spotify.com')
       .delete('/v1/users/U1AAAAAAA/playlists/P000000000000000000000/tracks')
       .reply(200);
@@ -43,15 +41,13 @@ describe('/eradicate interactive callback', function () {
 
     const body = require('../fixtures/eradicate_cancel.json');
 
-    chai
+    const res = await chai
       .request(app)
       .post('/interactive')
-      .send(body)
-      .end((err, res) => {
-        chai.assert.equal(res.body.text, 'Crisis averted.');
-        chai.assert.isFalse(removeTrackScope.isDone());
-        chai.assert.isFalse(nextTrackScope.isDone());
-        done();
-      });
+      .send(body);
+
+    chai.assert.equal(res.body.text, 'Crisis averted.');
+    chai.assert.isFalse(removeTrackScope.isDone());
+    chai.assert.isFalse(nextTrackScope.isDone());
   });
 });

test/slash_commands/eradicate.js
@@ -32,7 +32,8 @@ describe('/eradicate endpoint', function () {
               text: 'Do it.',
               type: 'button',
               style: 'danger',
-              value: '0eGsygTp906u18L0Oimnem'
+              value:
+                '{"spotifyID":"0eGsygTp906u18L0Oimnem","title":"Mr. Brightside","artist":"The Killers"}'
             },
             {
               name: 'cancel',